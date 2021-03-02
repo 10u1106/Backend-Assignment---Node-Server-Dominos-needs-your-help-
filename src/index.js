31809e8fc06456fb7a1c3f9b914b2377915e63d9
10u1106 <tanusaraswat26@gmail.com>
2021-03-03 00:00:52 +0530

    Edits Done!!

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-var http = require("http");
-
+var http = require('http');
+const port = 8081;
 const httpServer = http.createServer(handleServer);
-
+var text = 
 
 function handleServer(req, res) {
-  
+    res.setHeader('Content-Type', 'text/plain');
+    res.statusCode = 200
+    var url = req.url;
+
+    if(url == '/welcome'){
+        res.write('Welcome to Dominos!');
+        res.end();
+    }
+    else if(url == '/contact'){
+        res.write(`phone: '18602100000',
+        email: 'guestcaredominos@jublfood.com'`);
+        res.end();
+    }
+    else{
+        res.statusCode = 404;
+        end();
+    }
 }
 
+handleServer.listen(port);
 module.exports = httpServer;
