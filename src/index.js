a219019b32004ab9fbf45d0298bc6033c4e16438
10u1106 <tanusaraswat26@gmail.com>
2021-03-04 23:08:24 +0530

    final commit!!

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
 var http = require('http');
 const port = 8081;
 const httpServer = http.createServer(handleServer);
-var text = 
 
 function handleServer(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.statusCode = 200
+    // res.setHeader('Content-Type', 'text/plain');
+    // res.statusCode = 200
     var url = req.url;
 
-    if(url == '/welcome'){
+    if(url === '/welcome'){ 
+        res.setHeader('Content-Type', 'text/plain');
+        res.statusCode = 200
         res.write('Welcome to Dominos!');
         res.end();
     }
-    else if(url == '/contact'){
-        res.write(`phone: '18602100000',
-        email: 'guestcaredominos@jublfood.com'`);
+    else if(url === '/contact'){
+        res.setHeader('Content-Type', 'application/json');
+        res.statusCode = 200
+        res.write(JSON.stringify({phone: '18602100000',
+        email: 'guestcaredominos@jublfood.com'}));
         res.end();
     }
     else{
         res.statusCode = 404;
-        end();
+        res.end();
     }
 }
-
-handleServer.listen(port);
+httpServer.listen(port);
 module.exports = httpServer;
